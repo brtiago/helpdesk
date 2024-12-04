78b0c1fdd5cac8f3209550d007fdda481ac69b26
brtiago <tgribeiro@gmail.com>
2024-12-04 16:38:40 -0300

    feat(core): add global exception handler class

---
+++ src/main/java/com/tiago/Helpdesk/service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.tiago.Helpdesk.service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+}
