79071b2791f4c8d8ab02c47aab881397c98638e5
brtiago <tgribeiro@gmail.com>
2025-04-28 13:56:45 -0300

    feat(validation): enforce required fields with NotNull in TechnicianDTO

--- src/main/java/com/tiago/Helpdesk/controller/dto/TechnicianDTO.java
+++ src/main/java/com/tiago/Helpdesk/controller/dto/TechnicianDTO.java
@@ -1,15 +1,21 @@
 package com.tiago.Helpdesk.controller.dto;
 
 import com.tiago.Helpdesk.domain.Technician;
-import com.tiago.Helpdesk.enums.Profile;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.Set;
 
 public record TechnicianDTO(
         Integer id,
+        @NotNull(message = "The field Name is required.")
         String name,
+        @NotNull(message = "The field Cpf is required.")
         String cpf,
+        @NotNull(message = "The field Email is required.")
+        @Email
         String email,
+        @NotNull
         String password
         ) {
 
