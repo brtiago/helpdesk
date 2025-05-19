c6df68ac6b11233ed6af500247c31a377b5161a2
brtiago <tgribeiro@gmail.com>
2025-05-19 16:26:48 -0300

    feat(api): create User findall method

--- src/main/java/com/tiago/Helpdesk/service/UserService.java
+++ src/main/java/com/tiago/Helpdesk/service/UserService.java
@@ -1,4 +1,36 @@
 package com.tiago.Helpdesk.service;
 
+import com.tiago.Helpdesk.domain.User;
+import com.tiago.Helpdesk.repository.PersonRepository;
+import com.tiago.Helpdesk.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.lang.module.ResolutionException;
+import java.util.List;
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+    private final PersonRepository personRepository;
+
+    public UserService(UserRepository userRepository, PersonRepository personRepository) {
+        this.userRepository = userRepository;
+        this.personRepository = personRepository;
+    }
+
+    private void isIdValid(Integer id) {
+        if(id == null || id <= 0) {
+            throw new IllegalArgumentException("Invalid user id: " + id);
+        }
+    }
+
+    public User findById(Integer id) {
+        isIdValid(id);
+        return userRepository.findById(id).
+                orElseThrow(() -> new ResolutionException("User not found for ID: " + id));
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
 }
