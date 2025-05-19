3bb43d843e7f5dd5ce498dacf436255c50a1cb34
brtiago <tgribeiro@gmail.com>
2025-05-19 16:27:25 -0300

    feat(api): create User findall method

--- src/main/java/com/tiago/Helpdesk/controller/UserController.java
+++ src/main/java/com/tiago/Helpdesk/controller/UserController.java
@@ -3,6 +3,7 @@ package com.tiago.Helpdesk.controller;
 import com.tiago.Helpdesk.controller.dto.UserDTO;
 import com.tiago.Helpdesk.domain.User;
 import com.tiago.Helpdesk.repository.UserRepository;
+import com.tiago.Helpdesk.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,7 @@ public class UserController {
     private final UserService userService;
     private final UserRepository userRepository;
 
-    public UserController( UserService userService, UserRepository userRepository) {
+    public UserController(UserService userService, UserRepository userRepository) {
         this.userService = userService;
         this.userRepository = userRepository;
     }
