5b8c097c0df88de6c685cc8d1642d4d2a7c5405a
brtiago <tgribeiro@gmail.com>
2025-05-22 14:22:29 -0300

    feat(api): making a findall and create methods

--- src/main/java/com/tiago/Helpdesk/controller/UserController.java
+++ src/main/java/com/tiago/Helpdesk/controller/UserController.java
@@ -4,11 +4,15 @@ import com.tiago.Helpdesk.controller.dto.UserDTO;
 import com.tiago.Helpdesk.domain.User;
 import com.tiago.Helpdesk.repository.UserRepository;
 import com.tiago.Helpdesk.service.UserService;
+import jakarta.validation.Valid;
+import org.hibernate.validator.internal.constraintvalidators.bv.size.SizeValidatorForArray;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.method.support.UriComponentsContributor;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -16,10 +20,14 @@ public class UserController {
 
     private final UserService userService;
     private final UserRepository userRepository;
+    private final SizeValidatorForArray sizeValidatorForArray;
+    private final UriComponentsContributor uriComponentsContributor;
 
-    public UserController(UserService userService, UserRepository userRepository) {
+    public UserController(UserService userService, UserRepository userRepository, SizeValidatorForArray sizeValidatorForArray, UriComponentsContributor uriComponentsContributor) {
         this.userService = userService;
         this.userRepository = userRepository;
+        this.sizeValidatorForArray = sizeValidatorForArray;
+        this.uriComponentsContributor = uriComponentsContributor;
     }
 
     @GetMapping(value = "/{id}")
@@ -27,6 +35,27 @@ public class UserController {
         User user = userService.findById(id);
         UserDTO userDTO = new UserDTO(user);
         return ResponseEntity.ok().body(userDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<UserDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "10") Integer size) {
+
+        Pageable pageable = PageRequest.of(page, size);
+        Page<User> result = userRepository.findAll(pageable);
+        Page<UserDTO> userDTOPage = result.map(UserDTO::new);
+
+        return ResponseEntity.ok().body(userDTOPage);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> create(@Valid @RequestBody UserDTO userDTO, UriComponentsBuilder uriComponentsBuilder) {
+        var user = userService.create(userDTO);
+
+        var uri = uriComponentsBuilder.path("/users/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(new UserDTO(user));
+
     }
 
 
